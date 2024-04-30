906d9b5a2221d818a275f7ae18430c9d5293b8a7
candacecy0215 <146746922+candacecy0215@users.noreply.github.com>
2024-04-30 16:40:24 +0100

    Initial commit

---
+++ Assets/JC_StylizedNature_Lite/Prefabs/Box/boxbehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxbehaviour : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="Player"){
+            //play animation
+            animator.SetBool("openbox", true);
+        }
+        
+    }
+}
---
+++ Assets/JC_StylizedNature_Lite/Prefabs/drinkvessel/appearbahaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class appearbahaviour : MonoBehaviour
+{
+
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="Player"){
+            //play animation
+            animator.SetBool("appear", true);
+        }
+        
+    }
+}
